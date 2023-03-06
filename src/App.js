bc038b33ff304448acbcae7e523f9e79d36e906c
gozdemogus <gozdemog@gmail.com>
2023-03-06 19:45:36 +0300

    add components

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import Content from "./components/Content";
+
+function App() {
+  return (
+    <section class="todoapp">
+      <Header></Header>
+      <Content></Content>
+    </section>
+  );
+}
+
+export default App;
