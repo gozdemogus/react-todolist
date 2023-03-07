9fe372d52307f4f81cf4829b375544316baa2a71
gozdemogus <gozdemog@gmail.com>
2023-03-07 13:28:56 +0300

    delete todos

--- src/components/Content/List/Item.js
+++ src/components/Content/List/Item.js
@@ -3,10 +3,14 @@ import { useTodo } from "../../../contexts/TodoContext";
 
 function Item({ todo }) {
 
-  const { toggleTodo } = useTodo();
+  const { toggleTodo, destroyTodo } = useTodo();
   const onChange = (id) => {
     toggleTodo(id);
   };
+
+ const onDestroy = (id) => {
+  destroyTodo(id);
+ } 
 
   return (
     <li key={todo.id} className={todo.completed ? "completed" : ""}>
@@ -17,7 +21,7 @@ function Item({ todo }) {
           checked={todo.completed}
           onChange={() => onChange(todo.id)} />
         <label>{todo.text}</label>
-        <button className="destroy"></button>
+        <button className="destroy" onClick={() => onDestroy(todo.id)}></button>
       </div>
     </li>);
 }
--- src/contexts/TodoContext.js
+++ src/contexts/TodoContext.js
@@ -16,26 +16,38 @@ export const TodoProvider = ({ children }) => {
     }
     ])
 
-    const addTodo = (text) => 
-    setTodos((prev) => [...prev, { id: uuidv4(), completed: false, text: text }]);
+    const addTodo = (text) =>
+        setTodos((prev) => [...prev, { id: uuidv4(), completed: false, text: text }]);
 
-const toggleTodo = (id) => {
-    const cloned_todos = [...todos]; //referansını etkilememek icin cloneladı
-    const itemIndex = cloned_todos.findIndex(todo => todo.id === id);
-    console.log(itemIndex);
+    const toggleTodo = (id) => {
+        const cloned_todos = [...todos]; //referansını etkilememek icin cloneladı
+        const itemIndex = cloned_todos.findIndex(todo => todo.id === id);
+        console.log(itemIndex);
 
-    const item = todos[itemIndex];
-    console.log(item);
-    item.completed = !item.completed;
+        const item = todos[itemIndex];
+        console.log(item);
+        item.completed = !item.completed;
 
-    setTodos(cloned_todos);
-}
+        setTodos(cloned_todos);
+    };
+
+    const destroyTodo = (id) => {
+        const cloned_todos = [...todos];
+        const itemIndex = cloned_todos.findIndex((todo) => todo.id === id);
+        console.log(itemIndex);
+
+        cloned_todos.splice(itemIndex, 1);
+        console.log(cloned_todos);
+
+        setTodos(cloned_todos);
+    };
 
     const values = {
         todos,
         setTodos,
         addTodo,
-        toggleTodo
+        toggleTodo,
+        destroyTodo
     }
 
     return <TodoContext.Provider value={values}>{children} </TodoContext.Provider>
