2a46fbc2e9fa6d6214e2a27ed211449551206434
gozdemogus <gozdemog@gmail.com>
2023-03-07 12:44:53 +0300

    checkbox operations

--- src/contexts/TodoContext.js
+++ src/contexts/TodoContext.js
@@ -16,12 +16,26 @@ export const TodoProvider = ({ children }) => {
     }
     ])
 
-    const addTodo = (text) => setTodos((prev) => [...prev, { id: uuidv4(), completed: false, text: text }])
+    const addTodo = (text) => 
+    setTodos((prev) => [...prev, { id: uuidv4(), completed: false, text: text }]);
+
+const toggleTodo = (id) => {
+    const cloned_todos = [...todos]; //referansını etkilememek icin cloneladı
+    const itemIndex = cloned_todos.findIndex(todo => todo.id === id);
+    console.log(itemIndex);
+
+    const item = todos[itemIndex];
+    console.log(item);
+    item.completed = !item.completed;
+
+    setTodos(cloned_todos);
+}
 
     const values = {
         todos,
         setTodos,
-        addTodo
+        addTodo,
+        toggleTodo
     }
 
     return <TodoContext.Provider value={values}>{children} </TodoContext.Provider>
